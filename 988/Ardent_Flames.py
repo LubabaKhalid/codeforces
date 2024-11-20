07a1db5e9800c8c7b15f634f77264fde33b6230a
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2024-11-20 22:34:33 +0500

    added code

--- 988/Ardent_Flames.py
+++ 988/Ardent_Flames.py
@@ -1,17 +1,46 @@
-t=int(input())
-for i in range(t):
+def check(mid):
+    l=[]
+    r=[]
+    for i,j in enumerate(h):
+        req=(j+mid-1)//mid
+        if req>m:
+            continue
+        interval=m-req
+        l.append(x[i]-interval)
+        r.append(x[i]+interval+1)
+    l.sort()
+    r.sort()
+    i=0
+    cnt=0
+    for j in l:
+        cur=j
+        cnt+=1
+        while i<len(r) and cur>=r[i]:
+            cnt-=1
+            i+=1
+        if cnt>=k:
+            return True
+    return False
+ 
+def solve():
+    global n,m,k,h,x
     n,m,k=map(int,input().split())
-    h=list(map(int,input().split())) #n is length
-    x=list(map(int,input().split())) #n is length
-    damage=[]
-    for j in range(n):
-        totaldamage=0
-        for k in range(n):
-            dist=abs(x[j]-x[k])
-            if dist<m:
-                d=max(0,m - dist)
-                totaldamage=totaldamage+d
+    h=list(map(int,input().split()))
+    x=list(map(int,input().split()))
+    low=1
+    high=10**9
+    while high-low>1:
+        mid=(low+high)//2
+        if check(mid):
+            high=mid
+        else:
+            low=mid+1
+    if check(low):
+        print(low)
+    elif check(high):
+        print(high)
+    else:
+        print(-1)
         
-    if totaldamage>=h[i]:
-        damage.append(h[i])
-    print(damage)
+for _ in range(int(input())):
+    solve()

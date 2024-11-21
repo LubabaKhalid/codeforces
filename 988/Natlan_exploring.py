a8709f2d6a408d0277430183503474b2a2e002b9
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2024-11-21 15:38:42 +0500

    changes in natlan code

--- 988/Natlan_exploring.py
+++ 988/Natlan_exploring.py
@@ -1,12 +1,11 @@
 import math
+n= int(input())
+l=list(map(int,input().split()))
 m=998244353
-n=int(input())
-a=list(map(int,input().split()))
 dp=[0]*n
 dp[0]=1
 for i in range(n):
-    for j in range(i+1,n):
-        
-        if math.gcd(a[i],a[j])!=1:
+    for j in range(n):
+        if i<j and math.gcd(l[i],l[j])!=1:
             dp[j]=(dp[i]+dp[j])%m
 print(dp[n-1])

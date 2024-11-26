0e20ff0076a7cd2a8404bea3a86521f813c2916e
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2024-11-26 15:24:11 +0500

    added code

--- Halloumi Boxes.py
+++ Halloumi Boxes.py
@@ -1,13 +1,13 @@
 t=int(input())
 for _ in range(t):
     n,k=map(int,input().split())
-    l=list(map(int,input().split())) # length is n 
+    l=list(map(int,input().split()))  
     if l==sorted(l):
         print("YES")
         continue
+    
+    elif l>sorted(l) and k>=2:
+        print("YES")
     else:
-       if l>sorted(l) and k<2:
-           print("YES")
-       else:
-           print("NO")
+       print("NO")
              

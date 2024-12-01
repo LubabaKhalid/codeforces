efc1a4f33b063a1d93238f6e823b1b2fd1ae8446
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2024-12-01 20:09:32 +0500

    added code

--- Rayan/Rakhsh's Revival.py
+++ Rayan/Rakhsh's Revival.py
@@ -2,16 +2,19 @@ t=int(input())
 for _ in range(t):
     n,m,k=map(int,input().split())
     s=input()
-    z='0'*m
     c=0
     i=0
-    
-    while i <=(n-m):
-        if s[i:i+m]==z:
-            c+=1
-            i+=k
+    a=0
+    while i <len(s):
+        if s[i]!='0':
+            c=0
         else:
-            i+=1    
-    print(c)
+            c+=1
+        if c==m:
+            c=0
+            a+=1
+            i=i+k-1
+        i+=1   
+    print(a)
         
         

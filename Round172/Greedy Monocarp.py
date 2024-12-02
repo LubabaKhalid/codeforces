a06d6603dc0622aa5fe2722b6272db6104dd717d
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2024-12-02 21:28:58 +0500

    added code

--- Round172/Greedy Monocarp.py
+++ Round172/Greedy Monocarp.py
@@ -2,11 +2,27 @@ t=int(input())
 for _ in range(t):
     n,k=map(int,input().split())
     a=list(map(int,input().split()))
+    a.sort(reverse=True)
     ans=0
+    coins=0
+    flag=False
     for i in range(n):
-        ans+=a[i]
-        if ans==k:
+        if a[i]==k:
+            flag=True
             break
-        elif ans>k:
-            ans=ans-a[i]
-    print(k-ans)
+        elif a[i]==k-1:
+            flag=True
+            ans=1
+            break
+    if flag:
+        print(ans)
+    else:
+        for i in range(n):
+            ans+=a[i]
+            if ans==k:
+                break
+            elif ans>k:
+                ans-=a[i]
+        if ans<k:
+            coins=k-ans
+        print(coins)

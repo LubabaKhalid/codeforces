776aaacf7a3544baaf1ea5405316f6add8b335f4
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2024-12-04 00:13:51 +0500

    added changed  code

--- Round_990/Replace Character.py
+++ Round_990/Replace Character.py
@@ -2,6 +2,19 @@ from collections import Counter
 t=int(input())
 for i in range(t):
     n=int(input())
-    s=input()
+    s=list(input())
     count=Counter(s)
-    print(count)
+    mn=min(count.items(), key=lambda x:x[1])
+    mx=max(count.items(), key=lambda x:x[1])
+    if mn[0]!=mx[0]:
+        for j in range(n):
+            if mn[0]==s[j]:
+                s[j]=mx[0]
+                break
+    elif n>1:
+        for j in range(n-1):
+            if s[j]!=s[j+1]:
+                s[j+1]=s[j]
+                break
+        
+    print(''.join(s))

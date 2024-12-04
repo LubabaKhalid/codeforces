99706d88a2636bafbf759856b58a3011018dda03
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2024-12-04 09:44:01 +0500

    added changed  code

--- Round_990/Move Back at a Cost.py
+++ Round_990/Move Back at a Cost.py
@@ -1,20 +1,14 @@
-t=int(input())
+t=int(input())  
 for _ in range(t):
-    n=int(input())
-    a=list(map(int,input().split()))
-    i=0
-    while i<(n-1):
-        if a[i]>a[i+1] and i+1!=n-1:
-            a[i]=a[i]+1
-            temp=a[i]
-            for j in range(i,n-1):
-                a[j]=a[j+1]
-            a[n-1]=temp
-            i=0
-        elif a[i]>a[i+1] and i+1==n-1:
-            a[i],a[i+1]=a[i+1],a[i]
-            
-        i=i+1
+    n=int(input())  
+    a=list(map(int,input().split()))  
+    result=[]
+
     for i in range(n):
-        print(a[i],end=' ')  
-                
+        if i<n-1 and a[i]>a[i+1]:
+            a[i]+=1
+            result.append(a[i])
+        else:
+            result.append(a[i])
+    result.sort()
+    print(*result)

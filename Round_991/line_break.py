8eb4363da0ef7876fe3c3d30d5923eb737eaa560
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2024-12-05 19:56:33 +0500

    added code

--- Round_991/line_break.py
+++ Round_991/line_break.py
@@ -1,19 +1,15 @@
-def solve():
-    t = int(input())  # Read the number of test cases
-    for _ in range(t):
-        n, m = map(int, input().split())  # Read the number of words and the max length of the first strip
-        words = [input().strip() for _ in range(n)]  # Read the words
-        current_length = 0
-        x = 0
+t=int(input())  
+for _ in range(t):
+    n,m= map(int,input().split())  
+    words=[input().strip() for _ in range(n)]  
+    current_length=0
+    x=0
+    for word in words:
+        current_length+=len(word) 
+        if current_length>m:  
+            break
+        x+=1  
         
-        # Try to add words to the first strip until the length exceeds m
-        for word in words:
-            current_length += len(word)  # Add the length of the current word
-            if current_length > m:  # If the length exceeds the capacity of the strip
-                break
-            x += 1  # Increment x, meaning the word fits on the first strip
-        
-        print(x)  # Output the result for this test case
+    print(x) 
 
-# Run the solution
-solve()
+

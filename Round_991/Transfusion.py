88de792b63aa4c6ef74d7017e23da7de0a3e4f67
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2024-12-06 21:52:10 +0500

    added code

--- Round_991/Transfusion.py
+++ Round_991/Transfusion.py
@@ -1,9 +1,16 @@
 t=int(input())
-for i in range(t):
+for _ in range(t):
     n=int(input())
     a=list(map(int,input().split()))
-    ans=sum(a)
-    if ans%n==0:
+    s1=s2=0
+    for i in range(n):
+        if i%2==0:
+            s2+=a[i]
+        else:
+            s1+=a[i]
+    if s1%(n//2) or s2%((n+1)//2):
+        print("NO")
+    elif s1*((n+1)//2)==s2*(n//2):
         print("YES")
     else:
         print("NO")

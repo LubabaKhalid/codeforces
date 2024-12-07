2c8ee980f7a911ee90f574df8852c70d859979c9
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2024-12-07 23:26:30 +0500

    added code

---
+++ Round172/Competitive Fishing.py
@@ -0,0 +1,21 @@
+t=int(input())
+for _ in range(t):
+    n,k=map(int,input().split())
+    s=input()
+    arr=[0]*n
+    c=0
+    for i in range(n-1,-1,-1):
+        arr[i]=c
+        if s[i]=='1':
+            c+=1
+        else:
+            c-=1
+    arr.sort()
+    start=0
+    res=-1
+    for i in range(n-1,-1,-1):
+        start+=arr[i]
+        if start>=k:
+            res=n-i+1
+            break
+    print(res)

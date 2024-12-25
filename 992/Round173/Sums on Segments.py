d1d08652dbd97899c8460398df5197b7839b856d
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2024-12-25 13:30:12 +0500

    added change

--- 992/Round173/Sums on Segments.py
+++ 992/Round173/Sums on Segments.py
@@ -1,15 +1,24 @@
-t=int(input())
+def solve_case(a):
+    n=len(a)
+    sums=set()
+    prefix_sum=[0] * (n + 1)
+    for i in range(1, n + 1):
+        prefix_sum[i]=prefix_sum[i - 1] + a[i - 1]
+    
+    for i in range(n + 1):
+        for j in range(i + 1, n + 1):
+            sums.add(prefix_sum[j] - prefix_sum[i])
+    
+    sums.add(0)
+    result_sums=sorted(sums)
+    return len(result_sums), result_sums
+
+t = int(input())
+
 for _ in range(t):
     n=int(input())
-    a=list(map(int,input().split()))
-    b=a
-    b.append(0)
-    for i in range(n):
-        for j in range(i+1,n):
-            b.append(sum(a[i:j+1]))
-  
-    b=sorted(set(b))
-    print(len(b))
-    for i in b:
-        print(i,end=" ")
-    print()
+    a=list(map(int, input().split()))
+    
+    count,distinct_sums=solve_case(a)
+    print(count)
+    print(" ".join(map(str, distinct_sums)))

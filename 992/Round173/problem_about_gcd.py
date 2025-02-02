e19705451311b49f8bc8e01b758cb39c5b74eb1b
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2025-02-02 20:48:15 +0500

    added file

--- 992/Round173/problem_about_gcd.py
+++ 992/Round173/problem_about_gcd.py
@@ -4,25 +4,13 @@ for _ in range(t):
     s=[]
     c=0
     l,r,g=map(int,input().split())
-    for i in range(l,r+1):
-        if i%g==0:
-            s.append(i)
-            c=c+1
-            if c==2:
-                break
-            
-    if c==2:
-        m=math.gcd(s[0],s[1])
-        if m==g:
-            print(s[0],s[1])
-        else:
-            print(-1,-1)
-    elif c==1:
-        if s[0]==g:
-            print(s[0],s[0])
-        else:
-            print(-1,-1)
+    
+    if l%g==0:
+        a=l
+    else:
+        a=(l//g+1)*g
+    b=(r//g)*g
+    if a<=b and math.gcd(a,b)==g:
+        print(a,b)
     else:
         print(-1,-1)
-    
-            

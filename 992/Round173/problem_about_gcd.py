9301a1841ae12a5e439070bec04f2b2601bbf153
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2025-02-02 23:36:36 +0500

    added file

--- 992/Round173/problem_about_gcd.py
+++ 992/Round173/problem_about_gcd.py
@@ -1,4 +1,4 @@
-import math
+'''import math
 t=int(input())
 for _ in range(t):
     flag=False
@@ -19,6 +19,28 @@ for _ in range(t):
         if l<=g<=r:
             print(g,g)
         else:
-            print(-1,-1)
+            print(-1,-1)'''
+
+import math
+t=int(input())
+for _ in range(t):
+    l,r,g=map(int,input().split())
+    a=(l+g-1)//g
+    b=(r)//g
+    flag=False
+    for x in range(b-a+1):
+        for y in range(x+1):
+            n=a+y
+            m=b-x+y
+            if math.gcd(n,m)==1:
+                print(g*n,g*m)
+                flag=True
+                break
+        if flag:
+            break
+    if not flag:
+        print(-1,-1)
+                
+    
     
     

447ae9a5a68c75d53465292b6f1f79e2360ea018
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2025-02-02 22:21:28 +0500

    added file

--- 992/Round173/problem_about_gcd.py
+++ 992/Round173/problem_about_gcd.py
@@ -1,16 +1,24 @@
 import math
 t=int(input())
 for _ in range(t):
-    s=[]
-    c=0
+    flag=False
     l,r,g=map(int,input().split())
+    a=l
+    b=r
+    while a%g!=0:
+        a+=1
+    while b%g!=0:
+        b-=1
+    while b!=a:
+        if (math.gcd(a,b))==g:
+            flag=True
+            print(a,b)
+            break
+        b-=g
+    if flag is False:
+        if l<=g<=r:
+            print(g,g)
+        else:
+            print(-1,-1)
     
-    if l%g==0:
-        a=l
-    else:
-        a=(l//g+1)*g
-    b=(r//g)*g
-    if a<=b and math.gcd(a,b)==g:
-        print(a,b)
-    else:
-        print(-1,-1)
+    

b9136581d88841a5d1d1af035c325f1cc9b65438
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2025-02-04 00:41:40 +0500

    added file

--- 999/Kevin and Geometry.py
+++ 999/Kevin and Geometry.py
@@ -3,23 +3,26 @@ for _ in range(t):
     n=int(input())
     l=list(map(int,input().split()))
     l=sorted(l)
-    flag=False
-    a=0
-    b=0
-    c=0
-    for i in range(n-1):
-        if l[i]==l[i+1]:
-            c=2*l[i]
-            for j in range(i+1,n-1):
-                a=l[j]
-                b=l[j+1]
-                if c>abs(b-a):
-                    flag=True
-                    break
-            if flag:
-                break
-    if flag:
-        print(c//2,c//2,a,b)
+    if n>=4 and l[n-1]==l[n-2]:
+        print(l[n-1],l[n-2],l[n-3],l[n-4])
     else:
-        print(-1)
-        
+        flag=False
+        a=0
+        b=0
+        c=0
+        for i in range(n-1):
+            if l[i]==l[i+1]:
+                c=l[i]
+                for j in range(i+2,n-1):
+                    a=l[j]
+                    b=l[j+1]
+                    if (2*c)>abs(b-a):
+                        flag=True
+                        break
+                if flag:
+                    break
+        if flag:
+            print(c,c,a,b)
+        else:
+            print(-1)
+            

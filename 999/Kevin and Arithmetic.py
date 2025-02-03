ee8e5e58157c5b91b65326b93fb5990b1caf0d0e
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2025-02-03 23:28:20 +0500

    added file

--- 999/Kevin and Arithmetic.py
+++ 999/Kevin and Arithmetic.py
@@ -4,7 +4,7 @@ for _ in range(t):
     l=list(map(int,input().split()))
     s=0
     for i in range(n):
-        if l[i]%2==0:
+        if l[i]%2!=0:
             s=s+1
     if s==n:
         print(s-1)

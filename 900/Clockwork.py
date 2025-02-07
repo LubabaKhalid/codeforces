ad71c11f0ac9489c2283879fa9a42a61a6e665c1
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2025-02-07 22:49:56 +0500

    added file

--- 900/Clockwork.py
+++ 900/Clockwork.py
@@ -2,11 +2,13 @@ t=int(input())
 for _ in range(t):
     n=int(input())
     a=list(map(int,input().split()))
-    flag=False
-    s=0
+    c=0
     for i in range(n):
-        s=abs(s-a[i])
-    if s>1:
+        if a[i]>(2*max(i,(n-1)-i)):
+            c=c+1
+        else:
+            c=0
+    if c==n:
         print("YES")
     else:
         print("NO")

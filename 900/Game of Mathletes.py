bc788ee35e5ec17fc4f7c7024ba2009124560712
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2025-02-08 21:52:32 +0500

    added file

--- 900/Game of Mathletes.py
+++ 900/Game of Mathletes.py
@@ -4,18 +4,16 @@ for _ in range(t):
     a=list(map(int,input().split()))
     c=0
     a=sorted(a)
-    flag=False
-    for i in range(len(a)-1):
-        alice=a[i]
-        for j in range(i+1,len(a)):
-            bob=a[j]
-            if alice+bob==k:
-                c=c+1
-                del a[i]
-                del a[j]
-                flag=True
-                break
-        if not flag:
-            del a[i]
-            del a[i+1]
+    l=0
+    r=len(a)-1
+    while l<r:
+        x=a[l]+a[r]
+        if x==k:
+            c=c+1
+            l=l+1
+            r=r-1
+        elif x<k:
+            l=l+1
+        else:
+            r=r-1
     print(c)

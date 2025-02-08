76a64b0cf2678895de377b2f4ca2637ab91211f2
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2025-02-08 22:45:57 +0500

    added file

--- 800/MEX Table.py
+++ 800/MEX Table.py
@@ -1,15 +1,4 @@
 t=int(input())
 for _ in range(t):
     n,m=map(int,input().split())
-    x=n*m
-    c=1
-    n=0
-    if n==1:
-        print(m+1)
-    else:
-        for i in range(x):
-            if c==m+1:
-                n=i
-                break
-            c=c+1
-        print(n+1)
+    print(max(n,m)+1)

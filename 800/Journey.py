be48c53b182fb12f059ca4c396a04778b71a479d
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2025-02-12 17:50:03 +0500

    added file

--- 800/Journey.py
+++ 800/Journey.py
@@ -1,22 +1,18 @@
 t=int(input())
 for _ in range(t):
     n,a,b,c=map(int,input().split())
-    if a==1 and b==1 and c==1:
-        print(n)
-    else:
-        m=a+b+c
-        y=n%m
-        x=n//m
+    
+    m=a+b+c
+    y=n%m
+    x=n//m
+    c=0
+    if y==0:
         c=0
-        if y==0:
-            print(x*3)
-        else:
-            if y<=a:
-                c=c+1
-                y=y-a
-            if y<=b and y!=0:
-                c=c+1
-                y=y-b
-            if y<=c and y!=0:
-                c=c+1
-            print(c+(x*3))        
+    elif y<=a:
+        c=1
+    elif y<=(a+b):
+        c=2
+    else:
+        c=3
+    print(x*3+c)
+        

619a8a4f81706f9120db2a70661bf9fa4f049116
lubaba <bsdsf22a025@pucit.edu.pk>
2025-04-23 20:33:01 +0500

    added file

--- 900/Brr Brrr Patapim.py
+++ 900/Brr Brrr Patapim.py
@@ -5,12 +5,10 @@ for _ in range(t):
     for i in range(n):
         r=list(map(int,input().split()))
         x.append(r)
-    
-    ans=[]
-    for i in range(n):
-        for j in range(n):
-            if x[i][j] not in ans:
-                ans.append(x[i][j])
+    ans=x[0]
+    for i in range(1,n):
+        if x[i][-1] not in ans:
+            ans.append(x[i][-1])
     y=n*2
     for i in range(1,y+1):
         if i not in ans:

8b9b4abe492f511a85c4d27aa34775aac486c320
lubaba <bsdsf22a025@pucit.edu.pk>
2025-04-24 10:44:40 +0500

    added file

--- 900/Serval and String Theory.py
+++ 900/Serval and String Theory.py
@@ -1,9 +1,9 @@
 t=int(input())
 for _ in range(t):
     n,k=map(int,input().split())
-    s=input()
+    s=list(input())
     x=set(s)
-    if len(x)==1 or (k==0 and s>s[::-1]):
+    if len(x)==1 or (k==0 and s>=s[::-1]):
         print("NO")
     else:
         print("YES")

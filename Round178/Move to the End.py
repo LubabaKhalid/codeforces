5e81eac108d0907ac263eb2d6025809f3a45a370
lubaba <bsdsf22a025@pucit.edu.pk>
2025-04-28 20:49:02 +0500

    added file

--- Round178/Move to the End.py
+++ Round178/Move to the End.py
@@ -2,36 +2,21 @@ t=int(input())
 for _ in range(t):
     n=int(input())
     a=list(map(int,input().split()))
-    s=len(set(a))
-    if s==1:
-        for i in range(1,n+1):
-            print(a[0]*i,end=' ')
-        print()
-    elif s==2:
-        if a[0]>a[1]:
-            print(a[0],a[0]+a[1])
-        else:
-            print(a[1],a[1]+a[0])
-    else:
-        c=0
-        x=0
-        y=0
-        if n%2!=0:
-            y=n//2
-            x=y+1
-        else:
-            y=n//2
-            x=y
-        r=sorted(a[x-1:n])
-        l=sorted(a[0:y])
-        a=l+r
-        for i in range(n-1,-1,-1):
-            c=c+a[i]
-            print(c,end=' ')
+    s=[0]*(n+1)
+    for i in range(n-1,-1,-1):
+        s[n-i]=s[n-i-1]+a[i]
+    
+    res=[0]*n
+
+    for k in range(1, n + 1):
+        max_sum = 0
+        for i in range(n):
             
-            
-            
-                
-            
-        
-            
+            if k <= n:
+               
+                temp = a[:i]+a[i+1:]+[a[i]]
+                sum_k = sum(temp[-k:])
+                max_sum = max(max_sum, sum_k)
+        res[k-1]=max_sum
+
+    print(*res)

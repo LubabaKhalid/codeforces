a8538f57ae859fd9b1462b596a4314c187300410
lubaba <bsdsf22a025@pucit.edu.pk>
2025-04-28 20:50:26 +0500

    added file

--- Round178/Move to the End.py
+++ Round178/Move to the End.py
@@ -2,21 +2,11 @@ t=int(input())
 for _ in range(t):
     n=int(input())
     a=list(map(int,input().split()))
-    s=[0]*(n+1)
-    for i in range(n-1,-1,-1):
-        s[n-i]=s[n-i-1]+a[i]
+    a_sorted = sorted(a, reverse=True)
+    prefix=[]
+    total=0
+    for val in a_sorted:
+        total+=val
+        prefix.append(total)
     
-    res=[0]*n
-
-    for k in range(1, n + 1):
-        max_sum = 0
-        for i in range(n):
-            
-            if k <= n:
-               
-                temp = a[:i]+a[i+1:]+[a[i]]
-                sum_k = sum(temp[-k:])
-                max_sum = max(max_sum, sum_k)
-        res[k-1]=max_sum
-
-    print(*res)
+    print(*prefix)

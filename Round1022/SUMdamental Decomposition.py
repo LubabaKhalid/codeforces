d517f382605dba055482ab39901f4aaebbb82428
lubaba <bsdsf22a025@pucit.edu.pk>
2025-05-02 14:01:57 +0500

    added file

--- Round1022/SUMdamental Decomposition.py
+++ Round1022/SUMdamental Decomposition.py
@@ -1,18 +1,16 @@
 t=int(input())
 for _ in range(t):
     n,x=map(int,input().split())
-    if n==1:
-        if x==0:
-            print(-1)
-        else:
-            print(x)
-    elif n==2:
-        if x==1:
-            print(5)
-        elif x==0:
-            print(2)
-        else:
-            print(x)
-    
+    if n==1 and x==0:
+        print(-1)
     else:
-        print(x+n-1)      
+        a=bin(x)
+        c=a.count('1')
+        m=max(n-c,0)
+        if (n-c)>0 and (n-c)%2==1:
+            if x==0 or x==1:
+                m+=3
+            else:
+                m+=1
+        print(x+m)
+            
